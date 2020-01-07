9accd93fd1e8da6779fce89fdaa68853254cef1f
alexander-svedsen <alexandersvendsen1990@gmail.com>
2020-01-07 12:25:38 +0100

    FO-2655: Legg opp til configmap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import no.nav.apiapp.ApiApp;
+import no.nav.brukerdialog.tools.SecurityConstants;
+import no.nav.common.utils.NaisUtils;
 import no.nav.fo.veilarblest.config.ApplicationConfig;
+import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
+import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 
 import static java.lang.System.setProperty;
 import static no.nav.brukerdialog.security.Constants.OIDC_REDIRECT_URL_PROPERTY_NAME;
+import static no.nav.common.utils.NaisUtils.getCredentials;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarblest.config.ApplicationConfig.*;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
@@ -10,9 +15,44 @@ import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 public class Main {
 
     public static void main(String... args) {
+        readFromConfigMap();
+
+        NaisUtils.Credentials serviceUser = getCredentials("service_user");
+
+        //ABAC
+        System.setProperty(CredentialConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(CredentialConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //CXF
+        System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //OIDC
+        System.setProperty(SecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        System.setProperty(StsSecurityConstants.STS_URL_KEY, getRequiredProperty(SECURITYTOKENSERVICE_URL));
+
         setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty(AKTOER_V2_URL_PROPERTY));
         setProperty(OIDC_REDIRECT_URL_PROPERTY_NAME, getRequiredProperty(VEILARBLOGIN_REDIRECT_URL_URL_PROPERTY));
         ApiApp.runApp(ApplicationConfig.class, args);
+    }
+
+    private static void readFromConfigMap() {
+        NaisUtils.addConfigMapToEnv("pto-config",
+                "SECURITYTOKENSERVICE_URL",
+                "ISSO_HOST_URL",
+                "ISSO_JWKS_URL",
+                "ISSO_ISSUER_URL",
+                "ISSO_ISALIVE_URL",
+                "VEILARBLOGIN_REDIRECT_URL_DESCRIPTION",
+                "VEILARBLOGIN_REDIRECT_URL_URL",
+                "AKTOER_V2_SECURITYTOKEN",
+                "AKTOER_V2_ENDPOINTURL",
+                "AKTOER_V2_WSDLURL",
+                "LOGINSERVICE_OIDC_CALLBACKURI",
+                "LOGINSERVICE_OIDC_DISCOVERYURI"
+        );
     }
 
 }
