f1d697ea80295e5bd0b7141f5a20067710effc3d
stianStensli <stian.horn.stensli@nav.no>
2021-03-02 09:56:03 +0100

    postgres syntax

--- src/main/resources/db/migration/V1_4__kafka_feiltabell.sql
+++ src/main/resources/db/migration/V1_4__kafka_feiltabell.sql
@@ -1,7 +1,5 @@
-CREATE SEQUENCE FEILET_KAFKA_MELDING_SEQ START WITH 1;
-
 CREATE TABLE FEILET_KAFKA_MELDING (
-              ID                      SERIAL DEFAULT FEILET_KAFKA_MELDING_SEQ.nextval NOT NULL,
+              ID                      SERIAL,
               TOPIC_NAME              VARCHAR(200) NOT NULL,
               MESSAGE_KEY             VARCHAR(50) NOT NULL,
               JSON_PAYLOAD            CLOB NOT NULL
