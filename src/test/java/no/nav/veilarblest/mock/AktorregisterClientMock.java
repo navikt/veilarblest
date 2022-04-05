89edd84959eea9c1d0b539472bef36dcf1fad68f
marcjas <marc.joseph.ablian.saputil@nav.no>
2022-04-05 10:55:10 +0200

    bump commons og spring

--- src/test/java/no/nav/veilarblest/mock/AktorregisterClientMock.java
+++ src/test/java/no/nav/veilarblest/mock/AktorregisterClientMock.java
@@ -1,11 +1,15 @@
 package no.nav.veilarblest.mock;
 
+import no.nav.common.client.aktoroppslag.BrukerIdenter;
 import no.nav.common.client.aktorregister.AktorregisterClient;
-import no.nav.common.client.aktorregister.IdentOppslag;
+import no.nav.common.client.aktorregister.IngenGjeldendeIdentException;
 import no.nav.common.health.HealthCheckResult;
+import no.nav.common.types.identer.AktorId;
+import no.nav.common.types.identer.EksternBrukerId;
+import no.nav.common.types.identer.Fnr;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class AktorregisterClientMock implements AktorregisterClient {
 
@@ -14,31 +18,37 @@ public class AktorregisterClientMock implements AktorregisterClient {
     public final static String TEST_VEILEDER_IDENT = "Z12345";
 
     @Override
-    public String hentFnr(String aktorId) {
-        return TEST_FNR;
-    }
-
-    @Override
-    public String hentAktorId(String fnr) {
-        return TEST_AKTOR_ID;
-    }
-
-    @Override
-    public List<IdentOppslag> hentFnr(List<String> list) {
-        return list.stream()
-                .map(aktorId -> new IdentOppslag(aktorId, aktorId + "fnr"))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<IdentOppslag> hentAktorId(List<String> list) {
-        return list.stream()
-                .map(fnr -> new IdentOppslag(fnr, fnr + "aktorId"))
-                .collect(Collectors.toList());
-    }
-
-    @Override
     public HealthCheckResult checkHealth() {
         return HealthCheckResult.healthy();
+    }
+
+    @Override
+    public List<AktorId> hentAktorIder(Fnr fnr) {
+        return null;
+    }
+
+    @Override
+    public Fnr hentFnr(AktorId aktorId) throws IngenGjeldendeIdentException {
+        return Fnr.of(TEST_FNR);
+    }
+
+    @Override
+    public AktorId hentAktorId(Fnr fnr) throws IngenGjeldendeIdentException {
+        return AktorId.of(TEST_AKTOR_ID);
+    }
+
+    @Override
+    public Map<AktorId, Fnr> hentFnrBolk(List<AktorId> list) {
+        return null;
+    }
+
+    @Override
+    public Map<Fnr, AktorId> hentAktorIdBolk(List<Fnr> list) {
+        return null;
+    }
+
+    @Override
+    public BrukerIdenter hentIdenter(EksternBrukerId eksternBrukerId) {
+        return null;
     }
 }
